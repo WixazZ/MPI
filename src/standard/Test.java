21e8a66e54d9596fdff89af60b07e65c2bf2283d
mrkeinzi <hugo.rh@free.fr>
2021-05-02 22:49:06 +0200

    jack

--- src/standard/Test.java
+++ src/standard/Test.java
@@ -1,41 +1,14 @@
 package standard;
 import automaton.*;
+import reconnaissance.Reco;
 import java.io.*;
 import java.util.*;
 
+import static reconnaissance.Reco.read;
+
 public class Test {
 
-    public static boolean read(String mot, Automaton auto) {
-
-        Etat actif;
-
-        int j = 0;
-        while (j < auto.getNumberState() && !auto.getEtats()[j].getInit()) {
-            j++;
-        }
-        actif = auto.getEtats()[j];
-
-        for (int i = 0; i < mot.length(); i++) {
-
-            boolean find = false;
-            char x = mot.charAt(i);
-            int k = 0;
-            while(k < actif.getIndexOut() && !find){
-                if (actif.getOut()[k].getWord() == x) {
-                    find = true;
-                } else
-                    k++;
-            }
-            if (find == true)
-                actif = actif.getOut()[k].getArrive();
-            else
-                return false;
-        }
-        return actif.getFinish();
-    }
-
-
-        public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
 
         File fichier = new File("ressource/automate6.txt");
         Scanner lecteur = new Scanner(fichier);
@@ -55,6 +28,4 @@ public class Test {
         stand.getAFD().printAutomate();
         //autom.complementaire();*/
     }
-
-
 }
