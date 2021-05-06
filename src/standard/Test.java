4ef3f011135d587f14bc849669c1b829538d78f6
parad <alexis.paradiso@efrei.net>
2021-05-06 22:31:03 +0200

    ajout explication non standard

--- src/standard/Test.java
+++ src/standard/Test.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
 
-        File fichier = new File("ressource/automate4.txt");
+        File fichier = new File("ressource/automate3.txt");
         Scanner lecteur = new Scanner(fichier);
 
         Automaton autom = Lecteur.lecture(lecteur);
