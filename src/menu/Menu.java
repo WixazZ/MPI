fa93e5cf7d7a908a4c0314d143b441ea8fd9e190
Romain <romain.eybrard@gmail.com>
2021-05-07 17:55:17 +0200

    menu fichier

---
+++ src/menu/Menu.java
@@ -0,0 +1,28 @@
+package menu;
+
+public class Menu {
+
+    private boolean run = false;
+    public programRun(boolean run){
+        while (run != true) {
+            System.out.println("Veuillez entrer le numéro de l'automate que vous souhaitez utiliser et appuyez sur entrée");
+            switch (choix) {
+                case 1:
+
+                    break;
+                case 2:
+                    // Perform "encrypt number" case.
+                    break;
+                case 3:
+                    // Perform "decrypt number" case.
+                    break;
+                case 4:
+                    // Perform "quit" case.
+                    break;
+                default:
+                    // The user input an unexpected choice.
+            }
+
+        }
+    }
+}
