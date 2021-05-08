af44f356fc541f7a5d076cb9f32ee45ab39a34a2
parad <alexis.paradiso@efrei.net>
2021-05-09 00:24:29 +0200

    add menu

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -9,18 +9,31 @@ import java.util.Scanner;
 
 import static reconnaissance.Reco.read;
 
-public class test$ {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        boolean run = true;
-        while (run) {
+        boolean run = false;
+        while (!run) {
             String path2 = Menu.choixmenu();
             //System.out.println("/DEBUG/ path2 = " + path2);
             File fichier = new File(path2);
             Scanner lecteur = new Scanner(fichier);
 
             Automaton autom = Lecteur.lecture(lecteur);
-            autom.printAutomate();
-            System.out.println(read("abcd", autom));
+            int i=0;
+            String num = "";
+            while(path2.charAt(i) != '-'){
+                i++;
+            }
+            i++;
+            while(path2.charAt(i) != '.'){
+
+                num += path2.charAt(i);
+                i++;
+
+            }
+            int number = Integer.parseInt(num);
+            Trace.affiche(number, autom);
+            //System.out.println(read("abcd", autom));
 
             System.out.println("Voulez vous utiliser un autre automate\n 1 = oui \n 2 = non");
 
@@ -28,12 +41,14 @@ public class test$ {
             int choice = scanner.nextInt();
             while (choice !=1 && choice != 2){
                 System.out.println("VEUILLEZ ENTRER UN NOMBRE VALIDE!");
-                System.out.println("\n****************\n Voulez vous utiliser un autre automate ?\n 1 =oui \n 2 = non");
+                System.out.println("\n****************\n Voulez vous utiliser un autre automate ?\n 1 = oui \n 2 = non");
                 scanner = new Scanner(System.in);
                 choice = scanner.nextInt();
             }
-            if (choice == 2){
-                run = false;
+            switch (choice) {
+                case 1 -> run = false;
+                case 2 -> run = true;
+
             }
         }
     }
